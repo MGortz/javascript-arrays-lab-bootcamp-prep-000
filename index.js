dc286257bca9937a048327e4383ea9b61801078f
Mike Gorton <mike.gorton1@gmail.com>
2019-01-16 21:03:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(a) {
-  return kittens.push(a)
+  return kittens.push(a);
 }
 
 function destructivelyPrependKitten(a) {
-  return kittens.unshift(a)
+  return kittens.unshift(a);
 }
 
 function destructivelyRemoveLastKitten() {
-  return kittens.pop()
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
 }
